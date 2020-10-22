7aa243abf342a88e19924178f4425ef5361d5b0e
Ziga Patacko Koderman <ziga.patacko@gmail.com>
2020-10-22 09:52:53 +0200

    Implement gtk-layer-shellcalls

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -13,17 +13,29 @@ G_DEFINE_TYPE(MyApplication, my_application, GTK_TYPE_APPLICATION)
 // Implements GApplication::activate.
 static void my_application_activate(GApplication* application) {
   GtkWindow* window = GTK_WINDOW(gtk_application_window_new(GTK_APPLICATION(application)));
+
+  // layer shell config
+  // TODO: Causes Flutter to fail rendering any frames beyond the first one.
+  gtk_layer_init_for_window(GTK_WINDOW(window));
+  gtk_layer_set_layer (GTK_WINDOW(window), GTK_LAYER_SHELL_LAYER_BOTTOM);
+  gtk_layer_auto_exclusive_zone_enable (GTK_WINDOW(window));
+  gtk_layer_set_anchor(GTK_WINDOW(window), GTK_LAYER_SHELL_EDGE_BOTTOM, TRUE);
+  gtk_layer_set_anchor(GTK_WINDOW(window), GTK_LAYER_SHELL_EDGE_LEFT, TRUE);
+  gtk_layer_set_anchor(GTK_WINDOW(window), GTK_LAYER_SHELL_EDGE_RIGHT, TRUE);
+
   gtk_widget_show(GTK_WIDGET(window));
 
+  // run Flutter widget
   g_autoptr(FlDartProject) project = fl_dart_project_new();
-
   FlView* view = fl_view_new(project);
   gtk_widget_show(GTK_WIDGET(view));
+  // TODO: The two lines below cause crashes on mir_demo_server.
   gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(view));
-
   fl_register_plugins(FL_PLUGIN_REGISTRY(view));
 
-  gtk_widget_grab_focus(GTK_WIDGET(view));
+  // forces flutter to at least draw the first frame
+  // TODO: Not the way to do it.
+  gtk_widget_set_size_request (GTK_WIDGET(view), 100, 100);
 }
 
 static void my_application_class_init(MyApplicationClass* klass) {
